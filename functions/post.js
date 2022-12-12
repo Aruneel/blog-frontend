212de3d6afe7dd9d4e5476dc59e37691a5fa3b10
Aruneel Das <aruneel.das@arcadix.com>
2022-12-12 18:38:26 +0530

    added db post_store and api post

---
+++ functions/post.js
@@ -0,0 +1,14 @@
+import Store from '../backend/posts_store';
+
+export function onRequest(context) {
+    // return new Response(JSON.stringify(context.params.user))
+    const posts = new Store();
+    const postId = request.params.id;
+
+    const body = JSON.stringify(await posts.find(postId));
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'application/json',
+    };
+    return new Response(body, { headers });
+}
