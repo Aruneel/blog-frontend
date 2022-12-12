30c944fe494d07d6baa53d180bf5c831de534175
Aruneel Das <aruneel.das@arcadix.com>
2022-12-12 19:33:16 +0530

    .

--- functions/posts.js
+++ functions/posts.js
@@ -18,9 +18,10 @@ import Store from '../backend/posts_store';
 export const onRequest = async (context) => {
     const posts = new Store();
     const body = JSON.stringify(await posts.all());
+    let response = new Response(body)
     response.headers.set('Access-Control-Allow-Origin', '*');
     response.headers.set('Content-type', 'application/json');
-    response.body.set(body)
+    // response.body.set(body)
     return response;
   };
 
