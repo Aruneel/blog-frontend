413cbe8b642a936e9dc1c39880fe45be97e31d4f
Aruneel Das <aruneel.das@arcadix.com>
2022-12-12 19:17:48 +0530

    .

--- functions/posts.js
+++ functions/posts.js
@@ -19,7 +19,7 @@ export const onRequest = async (context) => {
     const posts = new Store();
     const body = JSON.stringify(await posts.all());
     response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Max-Age', '86400');
+    response.headers.set('Content-type', 'application/json');
     return  new Response(body, { headers });
   };
 
