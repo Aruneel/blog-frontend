6097f7c99ec09a3a0a1836d10e5cba7f52c4d43a
Aruneel Das <aruneel.das@arcadix.com>
2022-12-14 18:58:41 +0530

    .

--- functions/env/env.js
+++ functions/env/env.js
@@ -1,5 +1,6 @@
 export function onRequest(context) {
     let envVar = context.env.ENVIRONMENT
+    return new Response(envVar);
 	if (envVar == 'production') {
 		return new Response("production");
 	} else {
