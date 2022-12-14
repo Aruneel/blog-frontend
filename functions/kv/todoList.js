118582562ad062bd5588878353637372d3125b22
Aruneel Das <aruneel.das@arcadix.com>
2022-12-14 17:17:14 +0530

    .

--- functions/kv/todoList.js
+++ functions/kv/todoList.js
@@ -1,13 +1,5 @@
 export async function onRequest(context) {
-    const value = await context.env.KV_TODO_LIST.get("Task:123");
+    const task = JSON.stringify(await context.env.KV_TODO_LIST.get("Task:123"));
     console.log("task", task)
-    if (value === null) {
-        return new Response("Value not found", { status: 404 });
-      }
-    
-    return new Response(value);
-    // let response = new Response(task)
-    // response.headers.set('Access-Control-Allow-Origin', '*');
-    // response.headers.set('Content-type', 'application/json');
-    // return response;
+    return new Response(task);
 }
